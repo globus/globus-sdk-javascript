69c6676e800cdf48a53422e4796f1a999927815c
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2023-12-07 15:08:33 -0600

    feat: adds endpoint.remove and refactors test

--- src/lib/services/transfer/service/endpoint.ts
+++ src/lib/services/transfer/service/endpoint.ts
@@ -1,4 +1,4 @@
-import { serviceRequest } from '../../shared.js';
+import { HTTP_METHODS, serviceRequest } from '../../shared.js';
 
 import { ID, SCOPES } from '../config.js';
 
@@ -24,4 +24,27 @@ export const get = function (endpoint_xid, options?, sdkOptions?) {
     payload?: never;
   },
   JSONFetchResponse<Globus.Transfer.EndpointDocument>
+>;
+
+/**
+ * Delete an endpoint by its UUID.
+ * @see https://docs.globus.org/api/transfer/endpoint/#delete_endpoint_by_id
+ */
+export const remove = function (endpoint_xid, options?, sdkOptions?) {
+  return serviceRequest(
+    {
+      service: ID,
+      scope: SCOPES.ALL,
+      path: `/v0.10/endpoint/${endpoint_xid}`,
+      method: HTTP_METHODS.DELETE,
+    },
+    options,
+    sdkOptions,
+  );
+} satisfies ServiceMethodDynamicSegments<
+  string,
+  {
+    query?: never;
+    payload?: never;
+  }
 >;
