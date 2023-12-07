bf2577130bf7271d363640f9a02be165a575376a
Theron Cross <theroncross@gmail.com>
2023-12-07 12:15:47 -0800

    feat(GCS): added create, remove, update and patch methods for Storage Gateways (#42)

--- src/lib/services/globus-connect-server/__tests__/storage-gateways.spec.ts
+++ src/lib/services/globus-connect-server/__tests__/storage-gateways.spec.ts
@@ -4,20 +4,18 @@ import { storageGateways } from '..';
 import type { MirroredRequest } from '../../../../__mocks__/handlers';
 
 const GCS_HOST = 'https://fa5e.bd7c.data.globus.org';
+const GCS_CONFIGURATION = {
+  host: GCS_HOST,
+  endpoint_id: 'ac9cb54b-fc48-4824-b801-1388baf0a909',
+};
 
 describe('gcs – storageGateways', () => {
   createStorage('memory');
 
   test('getAll', async () => {
-    const result = await storageGateways.getAll(
-      {
-        host: GCS_HOST,
-        endpoint_id: 'ac9cb54b-fc48-4824-b801-1388baf0a909',
-      },
-      {
-        query: { include: ['accounts', 'private_policies'] },
-      },
-    );
+    const result = await storageGateways.getAll(GCS_CONFIGURATION, {
+      query: { include: ['accounts', 'private_policies'] },
+    });
     const {
       req: { url, method, headers },
     } = (await result.json()) as unknown as MirroredRequest;
@@ -41,13 +39,7 @@ describe('gcs – storageGateways', () => {
   });
 
   test('get', async () => {
-    const result = await storageGateways.get(
-      {
-        host: GCS_HOST,
-        endpoint_id: 'ac9cb54b-fc48-4824-b801-1388baf0a909',
-      },
-      'some-uuid',
-    );
+    const result = await storageGateways.get(GCS_CONFIGURATION, 'some-uuid');
     const {
       req: { url, method, headers },
     } = (await result.json()) as unknown as MirroredRequest;
@@ -65,6 +57,169 @@ describe('gcs – storageGateways', () => {
           "user-agent": "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)",
         },
         "method": "GET",
+        "url": "https://fa5e.bd7c.data.globus.org/api/storage_gateways/some-uuid",
+      }
+    `);
+  });
+
+  test('remove', async () => {
+    const result = await storageGateways.remove(GCS_CONFIGURATION, 'some-uuid', {
+      headers: {
+        Authorization: 'some-token',
+      },
+    });
+    const {
+      req: { url, method, headers },
+    } = (await result.json()) as unknown as MirroredRequest;
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchInlineSnapshot(`
+      {
+        "headers": {
+          "accept": "*/*",
+          "accept-encoding": "gzip,deflate",
+          "authorization": "some-token",
+          "connection": "close",
+          "host": "fa5e.bd7c.data.globus.org",
+          "user-agent": "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)",
+        },
+        "method": "DELETE",
+        "url": "https://fa5e.bd7c.data.globus.org/api/storage_gateways/some-uuid",
+      }
+    `);
+  });
+
+  test('create', async () => {
+    const result = await storageGateways.create(GCS_CONFIGURATION, {
+      headers: {
+        Authorization: 'Bearer an-example-token',
+      },
+      payload: {
+        DATA_TYPE: 'storage_gateway#1.2.0',
+        high_assurance: false,
+        allowed_domains: ['example.com'],
+        display_name: 'some name',
+        connector_id: 'some-connector-id',
+      },
+    });
+    const {
+      req: { url, method, headers, json },
+    } = (await result.json()) as unknown as MirroredRequest;
+    expect({
+      url,
+      method,
+      headers,
+      json,
+    }).toMatchInlineSnapshot(`
+      {
+        "headers": {
+          "accept": "*/*",
+          "accept-encoding": "gzip,deflate",
+          "authorization": "Bearer an-example-token",
+          "connection": "close",
+          "content-length": "156",
+          "content-type": "application/json",
+          "host": "fa5e.bd7c.data.globus.org",
+          "user-agent": "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)",
+        },
+        "json": {
+          "DATA_TYPE": "storage_gateway#1.2.0",
+          "allowed_domains": [
+            "example.com",
+          ],
+          "connector_id": "some-connector-id",
+          "display_name": "some name",
+          "high_assurance": false,
+        },
+        "method": "POST",
+        "url": "https://fa5e.bd7c.data.globus.org/api/storage_gateways",
+      }
+    `);
+  });
+
+  test('update', async () => {
+    const result = await storageGateways.update(GCS_CONFIGURATION, 'some-uuid', {
+      headers: {
+        Authorization: 'Bearer an-example-token',
+      },
+      payload: {
+        DATA_TYPE: 'storage_gateway#1.2.0',
+        allowed_domains: ['example.com'],
+        connector_id: 'some-connector-id',
+        display_name: 'some updated name',
+        high_assurance: false,
+      },
+    });
+    const {
+      req: { url, method, headers, json },
+    } = (await result.json()) as unknown as MirroredRequest;
+    expect({
+      url,
+      method,
+      headers,
+      json,
+    }).toMatchInlineSnapshot(`
+      {
+        "headers": {
+          "accept": "*/*",
+          "accept-encoding": "gzip,deflate",
+          "authorization": "Bearer an-example-token",
+          "connection": "close",
+          "content-length": "164",
+          "content-type": "application/json",
+          "host": "fa5e.bd7c.data.globus.org",
+          "user-agent": "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)",
+        },
+        "json": {
+          "DATA_TYPE": "storage_gateway#1.2.0",
+          "allowed_domains": [
+            "example.com",
+          ],
+          "connector_id": "some-connector-id",
+          "display_name": "some updated name",
+          "high_assurance": false,
+        },
+        "method": "PUT",
+        "url": "https://fa5e.bd7c.data.globus.org/api/storage_gateways/some-uuid",
+      }
+    `);
+  });
+
+  test('patch', async () => {
+    const result = await storageGateways.patch(GCS_CONFIGURATION, 'some-uuid', {
+      headers: {
+        Authorization: 'Bearer an-example-token',
+      },
+      payload: {
+        display_name: 'some patched name',
+      },
+    });
+    const {
+      req: { url, method, headers, json },
+    } = (await result.json()) as unknown as MirroredRequest;
+    expect({
+      url,
+      method,
+      headers,
+      json,
+    }).toMatchInlineSnapshot(`
+      {
+        "headers": {
+          "accept": "*/*",
+          "accept-encoding": "gzip,deflate",
+          "authorization": "Bearer an-example-token",
+          "connection": "close",
+          "content-length": "36",
+          "content-type": "application/json",
+          "host": "fa5e.bd7c.data.globus.org",
+          "user-agent": "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)",
+        },
+        "json": {
+          "display_name": "some patched name",
+        },
+        "method": "PATCH",
         "url": "https://fa5e.bd7c.data.globus.org/api/storage_gateways/some-uuid",
       }
     `);
--- src/lib/services/globus-connect-server/service/storage-gateways.ts
+++ src/lib/services/globus-connect-server/service/storage-gateways.ts
@@ -1,6 +1,6 @@
 import type { operations } from '@globus/types/gcs-manager/api';
 import { getRequiredScopes } from '../index.js';
-import { serviceRequest } from '../../../services/shared.js';
+import { HTTP_METHODS, serviceRequest } from '../../../services/shared.js';
 
 import type { GCSServiceMethod, GCSServiceMethodDynamicSegments } from '../index';
 
@@ -57,5 +57,124 @@ export const get = function (
   operations['getStorageGateway']['parameters']['path']['storage_gateway_id'],
   {
     query?: operations['getStorageGateway']['parameters']['query'];
+  }
+>;
+
+/**
+ * @see https://docs.globus.org/globus-connect-server/v5.4/api/openapi_Storage_Gateways/#deleteStorageGateway
+ */
+export const remove = function (
+  configuration,
+  storage_gateway_id,
+  options?,
+  sdkOptions?,
+): Promise<
+  JSONFetchResponse<
+    operations['deleteStorageGateway']['responses']['200']['content']['application/json']
+  >
+> {
+  return serviceRequest(
+    {
+      service: configuration,
+      scope: getRequiredScopes(configuration),
+      path: `/api/storage_gateways/${storage_gateway_id}`,
+      method: HTTP_METHODS.DELETE,
+    },
+    options,
+    sdkOptions,
+  );
+} satisfies GCSServiceMethodDynamicSegments<
+  operations['deleteStorageGateway']['parameters']['path']['storage_gateway_id'],
+  {
+    query?: never;
+    payload?: never;
+  }
+>;
+
+/**
+ * @see https://docs.globus.org/globus-connect-server/v5.4/api/openapi_Storage_Gateways/#postStorageGateway
+ */
+export const create = function (
+  configuration,
+  options,
+  sdkOptions?,
+): Promise<
+  JSONFetchResponse<
+    operations['postStorageGateway']['responses']['201']['content']['application/json']
+  >
+> {
+  return serviceRequest(
+    {
+      service: configuration,
+      scope: getRequiredScopes(configuration),
+      path: `/api/storage_gateways`,
+      method: HTTP_METHODS.POST,
+    },
+    options,
+    sdkOptions,
+  );
+} satisfies GCSServiceMethod<{
+  payload: operations['postStorageGateway']['requestBody']['content']['application/json'];
+}>;
+
+/**
+ * @see https://docs.globus.org/globus-connect-server/v5.4/api/openapi_Storage_Gateways/#putStorageGateway
+ */
+export const update = function (
+  configuration,
+  storage_gateway_id,
+  options,
+  sdkOptions?,
+): Promise<
+  JSONFetchResponse<
+    operations['putStorageGateway']['responses']['200']['content']['application/json']
+  >
+> {
+  return serviceRequest(
+    {
+      service: configuration,
+      scope: getRequiredScopes(configuration),
+      path: `/api/storage_gateways/${storage_gateway_id}`,
+      method: HTTP_METHODS.PUT,
+    },
+    options,
+    sdkOptions,
+  );
+} satisfies GCSServiceMethodDynamicSegments<
+  operations['putStorageGateway']['parameters']['path']['storage_gateway_id'],
+  {
+    payload: operations['putStorageGateway']['requestBody']['content']['application/json'];
+  }
+>;
+
+/**
+ * @see https://docs.globus.org/globus-connect-server/v5.4/api/openapi_Storage_Gateways/#patchStorageGateway
+ */
+export const patch = function (
+  configuration,
+  storage_gateway_id,
+  options,
+  sdkOptions?,
+): Promise<
+  JSONFetchResponse<
+    operations['patchStorageGateway']['responses']['200']['content']['application/json']
+  >
+> {
+  return serviceRequest(
+    {
+      service: configuration,
+      scope: getRequiredScopes(configuration),
+      path: `/api/storage_gateways/${storage_gateway_id}`,
+      method: HTTP_METHODS.PATCH,
+    },
+    options,
+    sdkOptions,
+  );
+} satisfies GCSServiceMethodDynamicSegments<
+  operations['patchStorageGateway']['parameters']['path']['storage_gateway_id'],
+  {
+    payload: Partial<
+      operations['patchStorageGateway']['requestBody']['content']['application/json']
+    >;
   }
 >;
