bcfdb9410fa2b1a5dbc0cd5a685308ab3974ae5b
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-03-07 21:20:50 -0600

    refactor: move AuthorizationManager to isolated file and start abstracting tokens behaviors...

---
+++ src/lib/core/authorization/tokens.ts
@@ -0,0 +1,21 @@
+import { Token } from '../../services/auth/index.js';
+
+import { getStorage } from '../storage/index.js';
+
+function isValidToken(check: unknown): check is Token {
+  const maybe = check as Token;
+  return Boolean(maybe.token_type && maybe.access_token);
+}
+
+/**
+ * Obtain the token string for the given scope.
+ * @param scope The scope string that will be used to look up the token.
+ * @returns The token string for the given scope or null if no token is found.
+ */
+export function getTokenForScope(scope: string) {
+  const token = getStorage().get(scope);
+  if (!token || !isValidToken(token)) {
+    return null;
+  }
+  return `${token.token_type} ${token.access_token}`;
+}
