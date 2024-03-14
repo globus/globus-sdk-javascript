16506e07ebb69a31e428635838db71a44dd78d1a
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-03-14 12:02:18 -0500

    test: adds AuthorizationManager.reset test

--- src/lib/core/__tests__/AuthorizationManager.spec.ts
+++ src/lib/core/__tests__/AuthorizationManager.spec.ts
@@ -6,10 +6,15 @@ import { Event } from '../authorization/Event';
 /**
  * Setup the local storage state for the tests.
  */
-export function setup(token?: Record<string, string>) {
-  jest
-    .spyOn(LocalStorage.prototype, 'get')
-    .mockImplementation(() => JSON.stringify(token || undefined));
+export function setup(state: Record<string, string> = {}) {
+  let storage = { ...state };
+  jest.spyOn(LocalStorage.prototype, 'get').mockImplementation((key: string) => storage[key]);
+  jest.spyOn(LocalStorage.prototype, 'set').mockImplementation((key: string, value: unknown) => {
+    storage[key] = JSON.stringify(value);
+  });
+  jest.spyOn(LocalStorage.prototype, 'clear').mockImplementation(() => {
+    storage = {};
+  });
 }
 
 describe('AuthorizationManager', () => {
@@ -45,18 +50,54 @@ describe('AuthorizationManager', () => {
   });
 
   it('should bootstrap from an existing token', () => {
-    setup({ token: 'example' });
+    setup({
+      'client_id:auth.globus.org': JSON.stringify({ resource_server: 'auth.globus.org' }),
+      'client_id:foobar': JSON.stringify({ resource_server: 'foobar' }),
+      'client_id:baz': JSON.stringify({ resource_server: 'baz' }),
+    });
     const spy = jest.spyOn(Event.prototype, 'dispatch');
     const instance = new AuthorizationManager({
       client_id: 'client_id',
       redirect_uri: 'https://redirect_uri',
       requested_scopes: 'foobar baz',
     });
+
     expect(spy).toHaveBeenCalledTimes(1);
     expect(spy).toHaveBeenCalledWith({
       isAuthenticated: true,
-      token: { token: 'example' },
+      token: { resource_server: 'auth.globus.org' },
     });
     expect(instance.authenticated).toBe(true);
+  });
+
+  it('supports reset', () => {
+    setup({
+      'client_id:auth.globus.org': JSON.stringify({ resource_server: 'auth.globus.org' }),
+      'client_id:foobar': JSON.stringify({ resource_server: 'foobar' }),
+      'client_id:baz': JSON.stringify({ resource_server: 'baz' }),
+    });
+    const instance = new AuthorizationManager({
+      client_id: 'client_id',
+      redirect_uri: 'https://redirect_uri',
+      requested_scopes: 'foobar baz',
+    });
+
+    expect(instance.authenticated).toBe(true);
+
+    const spy = jest.spyOn(Event.prototype, 'dispatch');
+    instance.reset();
+    /**
+     * `authenticated` should dispatch once with the initial state and again after reset.
+     */
+    expect(spy).toHaveBeenCalledTimes(2);
+    expect(spy).toHaveBeenNthCalledWith(1, {
+      isAuthenticated: true,
+      token: { resource_server: 'auth.globus.org' },
+    });
+    expect(spy).toHaveBeenNthCalledWith(2, {
+      isAuthenticated: false,
+      token: undefined,
+    });
+    expect(instance.authenticated).toBe(false);
   });
 });
