bd60bbecaa6c717fe3d00592706a7210c46f096c
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-04-04 14:57:58 -0500

    feat(Compute): adds initial support for Globus Compute
    - Adds `compute.endpoints.get` and `compute.endpoints.getAll`
    - Bumps @globus/types requirement to latest (0.0.7)

---
+++ src/lib/services/compute/__tests__/compute.spec.ts
@@ -0,0 +1,32 @@
+import { endpoints } from '../index';
+import { createStorage } from '../../../core/storage';
+
+import { mirror } from '../../../../__mocks__/handlers';
+
+describe('compute.endpoints', () => {
+  beforeEach(() => {
+    createStorage('memory');
+  });
+
+  test('getAll', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await endpoints.getAll());
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+
+  test('get', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await endpoints.get('some-uuid'));
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+});
---
+++ src/lib/services/compute/config.ts
@@ -0,0 +1,15 @@
+import type { Environment } from '../../core/global.js';
+
+export const ID = 'COMPUTE';
+export const HOSTS: Partial<Record<Environment, string>> = {
+  sandbox: 'compute.api.sandbox.globuscs.info',
+  production: 'compute.api.globus.org',
+  staging: 'compute.api.staging.globuscs.info',
+  integration: 'compute.api.integration.globuscs.info',
+  test: 'compute.api.test.globuscs.info',
+  preview: 'compute.api.preview.globus.org',
+};
+
+export const SCOPES = {
+  ALL: 'https://auth.globus.org/scopes/facd7ccc-c5f4-42aa-916b-a0e270e2c2a9/all',
+};
