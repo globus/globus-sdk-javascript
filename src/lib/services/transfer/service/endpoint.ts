d3ef97a0d464c8b9c3f544dd856211b7d5724389
Theron Cross <theroncross@gmail.com>
2024-04-19 11:32:28 -0700

    feat(Transfer): adds resource support for creation and update of GCP collections (#144)
    - `transfer.endpoint.create` can be used to create guest collections on
    a Globus Connect Personal mapped collection
    - `transfer.endpoint.update` can be used to update attributes on a
    mapped or guest collection

--- src/lib/services/transfer/service/endpoint.ts
+++ src/lib/services/transfer/service/endpoint.ts
@@ -1,8 +1,14 @@
 import { HTTP_METHODS, serviceRequest } from '../../shared.js';
 import { ID, SCOPES } from '../config.js';
 
-import type { ServiceMethodDynamicSegments, JSONFetchResponse } from '../../../services/types.js';
+import type {
+  ServiceMethodDynamicSegments,
+  JSONFetchResponse,
+  ServiceMethod,
+} from '../../../services/types.js';
+import type { Operations } from '../types.js';
 
+export type GetResponse = Operations['GetEndpoint']['response'];
 /**
  * Fetch an endpoint by its UUID.
  */
@@ -10,7 +16,7 @@ export const get = function (
   endpoint_xid,
   options?,
   sdkOptions?,
-): Promise<JSONFetchResponse<Globus.Transfer.EndpointDocument>> {
+): Promise<JSONFetchResponse<GetResponse>> {
   return serviceRequest(
     {
       service: ID,
@@ -28,6 +34,74 @@ export const get = function (
   }
 >;
 
+export type CreatePayload = Operations['CreateEndpoint']['payload'];
+export type CreateResponse = Operations['CreateEndpoint']['response'];
+/**
+ * Create a Globus Connect Personal guest collection.
+ * As of 2024-04-17, this method (and the Transfer API) only supports creating Globus Connect Personal guest collections.
+ *
+ * Globus Connect Personal mapped collections are created from the Globus Connect Personal application.
+ * All GCSv5 collections should be created using Globus Connect Server (i.e. {@link "@globus/sdk".gcs.collections.create | `gcs.collections.create`}).
+ *
+ * @see https://docs.globus.org/api/transfer/gcp_management/#create_guest_collection
+ */
+export const create = function (options?, sdkOptions?): Promise<JSONFetchResponse<CreateResponse>> {
+  if (options?.payload) {
+    Object.assign(options.payload, { DATA_TYPE: 'shared_endpoint' });
+  }
+
+  return serviceRequest(
+    {
+      service: ID,
+      scope: SCOPES.ALL,
+      path: '/v0.10/shared_endpoint',
+      method: HTTP_METHODS.POST,
+    },
+    options,
+    sdkOptions,
+  );
+} satisfies ServiceMethod<{
+  payload?: CreatePayload;
+}>;
+
+export type UpdatePayload = Operations['UpdateEndpoint']['payload'];
+export type UpdateResponse = Operations['UpdateEndpoint']['response'];
+/**
+ * Update a Globus Connect Personal collection.
+ * As of 2024-04-17, this method (and the Transfer API) only supports updating Globus Connect Personal collections.
+ *
+ * All GCSv5 collections should be updated using Globus Connect Server (i.e. {@link "@globus/sdk".gcs.collections.update | `gcs.collections.update`}).
+ *
+ * @see https://docs.globus.org/api/transfer/gcp_management/#update_collection_by_id
+ */
+export const update = function (
+  endpoint_xid,
+  options?,
+  sdkOptions?,
+): Promise<JSONFetchResponse<UpdateResponse>> {
+  if (options?.payload) {
+    Object.assign(options.payload, { DATA_TYPE: 'endpoint' });
+  }
+
+  return serviceRequest(
+    {
+      service: ID,
+      scope: SCOPES.ALL,
+      path: `/v0.10/endpoint/${endpoint_xid}`,
+      method: HTTP_METHODS.PUT,
+    },
+    options,
+    sdkOptions,
+  );
+} satisfies ServiceMethodDynamicSegments<
+  string,
+  {
+    payload: UpdatePayload;
+    query?: never;
+  }
+>;
+
+export type RemoveResponse = Operations['RemoveEndpoint']['response'];
 /**
  * Delete a Globus Connect Personal entity by its UUID.
  * As of 2024-01-08, this method (and the Transfer API) only supports deleting Globus Connect Personal entities.
@@ -40,15 +114,7 @@ export const remove = function (
   endpoint_xid,
   options?,
   sdkOptions?,
-): Promise<
-  JSONFetchResponse<{
-    DATA_TYPE: 'result';
-    code: 'Deleted';
-    message: string;
-    request_id: string;
-    resource: `/endpoint/${string}`;
-  }>
-> {
+): Promise<JSONFetchResponse<RemoveResponse>> {
   return serviceRequest(
     {
       service: ID,
