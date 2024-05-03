802efc1d62e51a505818c1d5d5f6ff1629eb9edc
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-05-02 20:56:20 -0500

    fix(Authorization): ensure AuthorizationManager.reset only clears values from storage that are prefixed with "<client_id>:"
    Prior to this change any calls to `.reset` would clear the underlying storage.

--- src/__mocks__/localStorage.ts
+++ src/__mocks__/localStorage.ts
@@ -1,24 +1,43 @@
-(function () {
-  let store: Record<string, string> = {};
-  Object.defineProperty(globalThis, 'localStorage', {
-    value: {
-      getItem: jest
-        .fn()
-        .mockImplementation((key) => (store[key] !== undefined ? store[key] : null)),
-      setItem: jest.fn().mockImplementation((key, value) => {
-        store[key] = `${value}`;
-      }),
-      removeItem: jest.fn().mockImplementation((key) => {
-        delete store[key];
-      }),
-      clear: jest.fn().mockImplementation(() => {
-        store = {};
-      }),
-    },
-    writable: true,
-  });
-})();
+class LocalStorageMock {
+  [key: string]: unknown;
 
+  constructor() {
+    Object.defineProperty(this, 'getItem', {
+      enumerable: false,
+      value: jest.fn((key) => (this[key] !== undefined ? this[key] : null)),
+    });
+
+    Object.defineProperty(this, 'setItem', {
+      enumerable: false,
+      value: jest.fn((key, value) => {
+        this[key] = `${value}`;
+      }),
+    });
+
+    Object.defineProperty(this, 'removeItem', {
+      enumerable: false,
+      value: jest.fn((key) => {
+        delete this[key];
+      }),
+    });
+
+    Object.defineProperty(this, 'clear', {
+      enumerable: false,
+      value: jest.fn(() => {
+        Object.keys(this).map((key) => delete this[key]);
+      }),
+    });
+  }
+
+  get length() {
+    return Object.keys(this).length;
+  }
+}
+
+Object.defineProperty(globalThis, 'localStorage', {
+  value: new LocalStorageMock(),
+  writable: true,
+});
 /**
  * Provide an object to set in (the mocked) `localStorage`
  */
