f4668494c79fc0045d76af53c0a7ec0a2bbacca3
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2024-08-02 14:30:19 -0500

    feat(Transfer): adds Transfer utilities
    - Transfer utiltities can be accessed via `sdk.transfer.utils`

---
+++ src/lib/services/transfer/__tests__/utils.spec.ts
@@ -0,0 +1,55 @@
+import { readableBytes, isDirectory, isFileDocument } from '../utils';
+
+describe('readableBytes', () => {
+  it('should return the correct readable string for bytes', () => {
+    expect(readableBytes(90)).toBe('90 B');
+    expect(readableBytes(1024)).toBe('1.02 KB');
+    expect(readableBytes(1048576)).toBe('1.05 MB');
+    expect(readableBytes(1073741824)).toBe('1.07 GB');
+    expect(readableBytes(1099511627776)).toBe('1.10 TB');
+    expect(readableBytes(1125899906842624)).toBe('1.13 PB');
+  });
+
+  it('should truncate the readable string based on the truncate parameter', () => {
+    expect(readableBytes(1024, 0)).toBe('1 KB');
+    expect(readableBytes(1048576, 1)).toBe('1.0 MB');
+    expect(readableBytes(1073741824, 3)).toBe('1.074 GB');
+  });
+});
+
+/**
+ * @see https://docs.globus.org/api/transfer/file_operations/#file_document
+ */
+const FILE = {
+  DATA_TYPE: 'file',
+  type: 'file',
+};
+
+const DIR = {
+  ...FILE,
+  type: 'dir',
+};
+
+const INVALID_SYMLINK = {
+  ...FILE,
+  type: 'invalid_symlink',
+};
+
+describe('isDirectory', () => {
+  it('should return true for directory objects', () => {
+    expect(isDirectory(DIR)).toBe(true);
+  });
+
+  it('should return false for non-directory objects', () => {
+    expect(isDirectory(FILE)).toBe(false);
+    expect(isDirectory(INVALID_SYMLINK)).toBe(false);
+  });
+});
+
+describe('isFileDocument', () => {
+  it('should return true for all "File Document" objects', () => {
+    expect(isFileDocument(FILE)).toBe(true);
+    expect(isFileDocument(DIR)).toBe(true);
+    expect(isFileDocument(INVALID_SYMLINK)).toBe(true);
+  });
+});
--- src/lib/services/transfer/utils.ts
+++ src/lib/services/transfer/utils.ts
@@ -1,5 +1,9 @@
-import type { FileDocument } from './service/file-operations';
+import type { FileDocument } from './service/file-operations.js';
 
+/**
+ * Determines if the given entry is a Globus Transfer File Document.
+ * @see https://docs.globus.org/api/transfer/file_operations/#file_document
+ */
 export function isFileDocument(entry: unknown): entry is FileDocument {
   return (
     typeof entry === 'object' &&
@@ -8,7 +12,46 @@ export function isFileDocument(entry: unknown): entry is FileDocument {
     entry.DATA_TYPE === 'file'
   );
 }
-
+/**
+ * Determines if the given entry is a directory.
+ * @see https://docs.globus.org/api/transfer/file_operations/#file_document
+ */
 export function isDirectory(entry: unknown) {
   return isFileDocument(entry) && entry.type === 'dir';
+}
+
+const KB = 1000;
+const MB = KB * 1000;
+const GB = MB * 1000;
+const TB = GB * 1000;
+const PB = TB * 1000;
+/**
+ * Returns a readable string for the given bytes, **rounded and truncated** to the given number of decimal places.
+ * @param bytes The number of bytes to convert to a readable string.
+ * @param [truncate=2] The number of decimal places to truncate the result to.
+ */
+export function readableBytes(bytes: number, truncate = 2) {
+  let unit = 'B';
+  let bytesInUnit = 1;
+  if (bytes < KB) {
+    return `${bytes} ${unit}`;
+  }
+  if (bytes < MB) {
+    unit = 'KB';
+    bytesInUnit = KB;
+  } else if (bytes < GB) {
+    unit = 'MB';
+    bytesInUnit = MB;
+  } else if (bytes < TB) {
+    unit = 'GB';
+    bytesInUnit = GB;
+  } else if (bytes < PB) {
+    unit = 'TB';
+    bytesInUnit = TB;
+  } else {
+    unit = 'PB';
+    bytesInUnit = PB;
+  }
+  const value = bytes / bytesInUnit;
+  return `${value.toFixed(truncate)} ${unit}`;
 }
