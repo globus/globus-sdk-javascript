bd051cba07fff3c39b1bd13acadc0e4531dab393
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2025-01-06 09:16:26 -0600

    feat(Auth): Adds `auth.identities.consents.getAll` service method. (#392)
    chore(Auth): Updates the `auth.identities` direct import path to `@globus/sdk/services/auth/identities/index.js`
    chore(Auth, Typescript): adds `Consent` type.

--- src/services/auth/__tests__/identities.spec.ts
+++ src/services/auth/__tests__/identities.spec.ts
@@ -1,12 +1,12 @@
 import { identities } from '..';
 
-import type { MirroredRequest } from '../../../__mocks__/handlers';
+import { mirror } from '../../../__mocks__/handlers';
 
 test('get', async () => {
   const result = await identities.get('6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9');
   const {
     req: { url, method, headers },
-  } = (await result.json()) as unknown as MirroredRequest;
+  } = await mirror(result);
 
   expect({
     url,
@@ -35,7 +35,7 @@ test('getAll', async () => {
   });
   const {
     req: { url, method, headers },
-  } = (await result.json()) as unknown as MirroredRequest;
+  } = await mirror(result);
 
   expect({
     url,
@@ -65,7 +65,7 @@ test("getAll – with 'include'", async () => {
   });
   const {
     req: { url, method, headers },
-  } = (await result.json()) as unknown as MirroredRequest;
+  } = await mirror(result);
 
   expect({
     url,
@@ -82,6 +82,31 @@ test("getAll – with 'include'", async () => {
   },
   "method": "GET",
   "url": "https://auth.globus.org/identities?ids=538e096f-0468-4d54-8463-50af72c01f95%2C1e1cac10-b303-48d8-aa81-3b3a592a2564&include=identity_provider",
+}
+`);
+});
+
+test('consents', async () => {
+  const result = await identities.consents.getAll('6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9');
+  const {
+    req: { url, method, headers },
+  } = await mirror(result);
+
+  expect({
+    url,
+    method,
+    headers,
+  }).toMatchInlineSnapshot(`
+{
+  "headers": {
+    "accept": "*/*",
+    "accept-encoding": "gzip,deflate",
+    "connection": "close",
+    "host": "auth.globus.org",
+    "user-agent": "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)",
+  },
+  "method": "GET",
+  "url": "https://auth.globus.org/identities/6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9/consents",
 }
 `);
 });
