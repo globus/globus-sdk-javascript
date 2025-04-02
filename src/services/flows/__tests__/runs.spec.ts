0712eeeb57a94c3ef92b41d44607a7c83276ae5d
jakeglobus <76002510+jakeglobus@users.noreply.github.com>
2025-04-02 15:12:57 -0500

    feat(Flows): Add Run `update`, `remove`, and `getDefinition` methods (#473)

--- src/services/flows/__tests__/runs.spec.ts
+++ src/services/flows/__tests__/runs.spec.ts
@@ -54,4 +54,46 @@ describe('flows.runs', () => {
       }).toMatchSnapshot();
     });
   });
+
+  test('update', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(
+      await runs.update('run-id', {
+        payload: {
+          label: 'abc',
+          run_managers: ['urn:globus:auth:identity:uuid'],
+          run_monitors: ['urn:globus:auth:identity:uuid'],
+          tags: ['tag1', 'tag2'],
+        },
+      }),
+    );
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+
+  test('remove', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await runs.remove('run-id'));
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+
+  test('getDefinition', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await runs.getDefinition('run-id'));
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
 });
