625efa5042ef067647b59c3856d07bcc76cdf025
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2025-05-29 15:52:21 -0500

    chore: generate .ts files instead of definition files to ensure inclusion in build

--- scripts/open-api/generate-types.mjs
+++ scripts/open-api/generate-types.mjs
@@ -12,23 +12,23 @@ const OPEN_API_TYPES_DIR = path.resolve(import.meta.dirname, '../../src/open-api
 const SCHEMAS = [
   {
     url: 'https://groups.api.globus.org/openapi.json',
-    filename: path.join(OPEN_API_TYPES_DIR, `groups.d.ts`),
+    filename: path.join(OPEN_API_TYPES_DIR, `groups.ts`),
   },
   {
     url: 'https://timer.automate.globus.org/openapi.json',
-    filename: path.join(OPEN_API_TYPES_DIR, `timer.d.ts`),
+    filename: path.join(OPEN_API_TYPES_DIR, `timer.ts`),
   },
   {
     url: 'https://api2.funcx.org/openapi.json',
-    filename: path.join(OPEN_API_TYPES_DIR, `compute.d.ts`),
+    filename: path.join(OPEN_API_TYPES_DIR, `compute.ts`),
   },
   {
     url: 'https://docs.globus.org/globus-connect-server/v5.4/api/api.js',
-    filename: path.join(OPEN_API_TYPES_DIR, `gcs/v5.4.d.ts`),
+    filename: path.join(OPEN_API_TYPES_DIR, `gcs/v5.4.ts`),
   },
   {
     url: 'https://search.api.globus.org/autodoc/openapi.json',
-    filename: path.join(OPEN_API_TYPES_DIR, `search.d.ts`),
+    filename: path.join(OPEN_API_TYPES_DIR, `search.ts`),
   },
 ];
 
