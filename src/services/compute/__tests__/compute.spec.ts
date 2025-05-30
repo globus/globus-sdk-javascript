36a940e30fb5a5269160ee5e971cf124ca3f7fcc
Theron Cross <theroncross@gmail.com>
2025-05-30 07:34:56 -0700

    feat(Compute): adds endpoint update and delete methods

--- src/services/compute/__tests__/compute.spec.ts
+++ src/services/compute/__tests__/compute.spec.ts
@@ -35,4 +35,32 @@ describe('compute.endpoints', () => {
       headers,
     }).toMatchSnapshot();
   });
+
+  test('update', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(
+      await endpoints.update('some-uuid', {
+        payload: {
+          display_name: 'New Display Name',
+        },
+      }),
+    );
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+
+  test('delete', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await endpoints.remove('some-uuid'));
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
 });
