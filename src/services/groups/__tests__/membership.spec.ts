5d28d0b0bc8a7aa0ba42d5bedfb55bf2d3b17bef
Theron Cross <theroncross@gmail.com>
2025-09-09 10:11:46 -0700

    feat(Groups): add missing service methods
    - group
      - create
      - update
      - remove
    - membership
      - getStatuses
    - policies
      - update
    
    [sc-44804][sc-44800][sc-44801]

--- src/services/groups/__tests__/membership.spec.ts
+++ src/services/groups/__tests__/membership.spec.ts
@@ -1,62 +1,46 @@
 import { membership } from '..';
 
-import type { MirroredRequest } from '../../../__mocks__/handlers';
+import { mirror } from '../../../__mocks__/handlers';
 
 test('membership - act', async () => {
-  const result = await membership.act('6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9', {
-    payload: {
-      reject: [
-        {
-          identity_id: 'f13c9c26-223d-41cf-b6d7-971f3d3b5dfa',
-        },
-      ],
-      decline: [
-        {
-          identity_id: 'c237bf3e-ff0b-4d7f-9abc-5355040c49da',
-        },
-        {
-          identity_id: '3d9cbcd8-beed-4f37-85ab-75d725623da8',
-        },
-      ],
-    },
-  });
   const {
     req: { url, method, headers, json },
-  } = (await result.json()) as unknown as MirroredRequest;
+  } = await mirror(
+    await membership.act('6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9', {
+      payload: {
+        reject: [
+          {
+            identity_id: 'f13c9c26-223d-41cf-b6d7-971f3d3b5dfa',
+          },
+        ],
+        decline: [
+          {
+            identity_id: 'c237bf3e-ff0b-4d7f-9abc-5355040c49da',
+          },
+          {
+            identity_id: '3d9cbcd8-beed-4f37-85ab-75d725623da8',
+          },
+        ],
+      },
+    }),
+  );
 
   expect({
     url,
     method,
     headers,
     json,
-  }).toMatchInlineSnapshot(`
-    {
-      "headers": {
-        "accept": "*/*",
-        "accept-encoding": "gzip,deflate",
-        "connection": "close",
-        "content-length": "189",
-        "content-type": "application/json",
-        "host": "groups.api.globus.org",
-        "user-agent": "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)",
-      },
-      "json": {
-        "decline": [
-          {
-            "identity_id": "c237bf3e-ff0b-4d7f-9abc-5355040c49da",
-          },
-          {
-            "identity_id": "3d9cbcd8-beed-4f37-85ab-75d725623da8",
-          },
-        ],
-        "reject": [
-          {
-            "identity_id": "f13c9c26-223d-41cf-b6d7-971f3d3b5dfa",
-          },
-        ],
-      },
-      "method": "POST",
-      "url": "https://groups.api.globus.org/v2/groups/6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9",
-    }
-  `);
+  }).toMatchSnapshot();
+});
+
+test('getStatuses', async () => {
+  const {
+    req: { url, method, headers },
+  } = await mirror(await membership.getStatuses());
+
+  expect({
+    url,
+    method,
+    headers,
+  }).toMatchSnapshot();
 });
--- src/services/groups/service/membership.ts
+++ src/services/groups/service/membership.ts
@@ -1,8 +1,13 @@
-import { ID, SCOPES } from '../config.js';
+import { ID } from '../config.js';
 import { HTTP_METHODS, serviceRequest } from '../../shared.js';
+import { RESOURCE_SERVERS } from '../../auth/config.js';
 
 import type { OpenAPI } from '../index.js';
-import type { JSONFetchResponse, ServiceMethodDynamicSegments } from '../../types.js';
+import type {
+  JSONFetchResponse,
+  ServiceMethod,
+  ServiceMethodDynamicSegments,
+} from '../../types.js';
 
 /**
  * Perform actions on members of the group.
@@ -17,11 +22,10 @@ export const act = function (
     OpenAPI.operations['group_membership_post_actions_v2_groups__group_id__post']['responses']['200']['content']['application/json']
   >
 > {
-  if (!options?.payload) throw new Error('payload is required.');
   return serviceRequest(
     {
       service: ID,
-      scope: SCOPES.ALL,
+      resource_server: RESOURCE_SERVERS.GROUPS,
       path: `/v2/groups/${group_id}`,
       method: HTTP_METHODS.POST,
     },
@@ -31,6 +35,33 @@ export const act = function (
 } satisfies ServiceMethodDynamicSegments<
   OpenAPI.operations['update_group_v2_groups__group_id__put']['parameters']['path']['group_id'],
   {
+    query?: never;
     payload: OpenAPI.operations['group_membership_post_actions_v2_groups__group_id__post']['requestBody']['content']['application/json'];
   }
 >;
+
+/**
+ * Get the status counts of memberships for each group you are an admin or manager of.
+ * @see https://groups.api.globus.org/redoc#tag/groups/operation/get_statuses_v2_groups_statuses_get
+ */
+export const getStatuses = function (
+  options?,
+  sdkOptions?,
+): Promise<
+  JSONFetchResponse<
+    OpenAPI.operations['get_statuses_v2_groups_statuses_get']['responses']['200']['content']['application/json']
+  >
+> {
+  return serviceRequest(
+    {
+      service: ID,
+      resource_server: RESOURCE_SERVERS.GROUPS,
+      path: `/v2/groups/statuses`,
+    },
+    options,
+    sdkOptions,
+  );
+} satisfies ServiceMethod<{
+  query?: never;
+  payload?: never;
+}>;
