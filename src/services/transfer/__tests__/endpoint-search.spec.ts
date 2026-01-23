2afde472d99808fde7d88e2e84342fdb25d913c5
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2026-01-23 12:35:29 -0700

    fix(Transfer): Updates Transfer API host to transfer.api.globus.org (#732)
    Relates to globusonline/docs.globus.org#2035

--- src/services/transfer/__tests__/endpoint-search.spec.ts
+++ src/services/transfer/__tests__/endpoint-search.spec.ts
@@ -25,11 +25,11 @@ test('endpointSearch', async () => {
         "accept-encoding": "gzip,deflate",
         "authorization": "Bearer an-example-token",
         "connection": "close",
-        "host": "transfer.api.globusonline.org",
+        "host": "transfer.api.globus.org",
         "user-agent": "node-fetch/1.0 (+https://github.com/bitinn/node-fetch)",
       },
       "method": "GET",
-      "url": "https://transfer.api.globusonline.org/v0.10/endpoint_search?filter_fulltext=my+search+string&limit=10",
+      "url": "https://transfer.api.globus.org/v0.10/endpoint_search?filter_fulltext=my+search+string&limit=10",
     }
   `);
 });
