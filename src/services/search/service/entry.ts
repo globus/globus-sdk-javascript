ae9d4a2aa9321fcd52262fe517cd9f09f80ddc2b
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2026-02-17 09:28:33 -0600

    feat(Search): Adds `next` exports to Search resources (#756)
    ~Depends on #753 (currently showing _that_ diff, but will be merged to `main`)~

--- src/services/search/service/entry.ts
+++ src/services/search/service/entry.ts
@@ -1,5 +1,7 @@
 import { serviceRequest } from '../../shared.js';
 import { ID, SCOPES } from '../config.js';
+import { RESOURCE_SERVERS } from '../../auth/config.js';
+import { createServiceMethodFactory } from '../../factory.js';
 
 import type { ResultFormatVersion } from '../types.js';
 import type { ServiceMethodDynamicSegments } from '../../types.js';
@@ -34,3 +36,29 @@ export const get = function (index_id, options?, sdkOptions?) {
     };
   }
 >;
+
+/**
+ * @private
+ */
+export const next = {
+  /**
+   * Lookup a single Entry in a Search Index.
+   * @param index_id The UUID of the Search index where the lookup will be performed.
+   *
+   * @see https://docs.globus.org/api/search/reference/get_entry/
+   */
+  get: createServiceMethodFactory({
+    service: ID,
+    resource_server: RESOURCE_SERVERS[ID],
+    path: `/v1/index/{index_id}/entry`,
+  }).generate<{
+    request: {
+      query: {
+        subject: string;
+        entry_id?: string;
+        result_format_version?: ResultFormatVersion | string;
+        bypass_visible_to?: 'true' | 'false';
+      };
+    };
+  }>(),
+};
