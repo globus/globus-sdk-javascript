89c16c3322295de4fd382d74bcc989f407c40c9c
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2026-02-17 14:13:59 -0600

    feat(Groups): Adds `next` exports to Groups resources (#757)
    ~Depends on #753 (currently showing _that_ diff, but will be merged to
    `main`)~

--- src/services/groups/__tests__/membership.spec.ts
+++ src/services/groups/__tests__/membership.spec.ts
@@ -2,26 +2,30 @@ import { membership } from '..';
 
 import { mirror } from '../../../__mocks__/handlers';
 
+const GROUP_ID = '6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9';
+
+const ACT_PAYLOAD = {
+  reject: [
+    {
+      identity_id: 'f13c9c26-223d-41cf-b6d7-971f3d3b5dfa',
+    },
+  ],
+  decline: [
+    {
+      identity_id: 'c237bf3e-ff0b-4d7f-9abc-5355040c49da',
+    },
+    {
+      identity_id: '3d9cbcd8-beed-4f37-85ab-75d725623da8',
+    },
+  ],
+};
+
 test('membership - act', async () => {
   const {
     req: { url, method, headers, json },
   } = await mirror(
-    await membership.act('6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9', {
-      payload: {
-        reject: [
-          {
-            identity_id: 'f13c9c26-223d-41cf-b6d7-971f3d3b5dfa',
-          },
-        ],
-        decline: [
-          {
-            identity_id: 'c237bf3e-ff0b-4d7f-9abc-5355040c49da',
-          },
-          {
-            identity_id: '3d9cbcd8-beed-4f37-85ab-75d725623da8',
-          },
-        ],
-      },
+    await membership.act(GROUP_ID, {
+      payload: ACT_PAYLOAD,
     }),
   );
 
@@ -31,4 +35,25 @@ test('membership - act', async () => {
     headers,
     json,
   }).toMatchSnapshot();
+});
+
+describe('next', () => {
+  test('act', async () => {
+    const {
+      req: { url, method, headers, json },
+    } = await mirror(
+      await membership.next.act({
+        group_id: GROUP_ID,
+        request: {
+          data: ACT_PAYLOAD,
+        },
+      }),
+    );
+    expect({
+      url,
+      method,
+      headers,
+      json,
+    }).toMatchSnapshot();
+  });
 });
--- src/services/groups/__tests__/policies.spec.ts
+++ src/services/groups/__tests__/policies.spec.ts
@@ -2,10 +2,12 @@ import { policies } from '..';
 
 import { mirror } from '../../../__mocks__/handlers';
 
+const GROUP_ID = '6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9';
+
 test('get', async () => {
   const {
     req: { url, method, headers },
-  } = await mirror(await policies.get('6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9'));
+  } = await mirror(await policies.get(GROUP_ID));
 
   expect({
     url,
@@ -18,7 +20,7 @@ test('update', async () => {
   const {
     req: { url, method, headers, json },
   } = await mirror(
-    await policies.update('6521a0c3-ffc9-4432-9cb6-41fa8fe2e4e9', {
+    await policies.update(GROUP_ID, {
       payload: {
         is_high_assurance: true,
       },
@@ -31,4 +33,38 @@ test('update', async () => {
     headers,
     json,
   }).toMatchSnapshot();
+});
+
+describe('next', () => {
+  test('get', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await policies.next.get({ group_id: GROUP_ID }));
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+
+  test('update', async () => {
+    const {
+      req: { url, method, headers, json },
+    } = await mirror(
+      await policies.next.update({
+        group_id: GROUP_ID,
+        request: {
+          data: {
+            is_high_assurance: true,
+          },
+        },
+      }),
+    );
+    expect({
+      url,
+      method,
+      headers,
+      json,
+    }).toMatchSnapshot();
+  });
 });
