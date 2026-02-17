ab6b2abf8fadde982500e360948bdc9af4588845
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2026-02-17 14:15:05 -0600

    feat(Compute): Adds `next` exports to Compute resources (#760)

--- src/services/compute/__tests__/compute.spec.ts
+++ src/services/compute/__tests__/compute.spec.ts
@@ -2,6 +2,8 @@ import { endpoints } from '../index';
 
 import { mirror } from '../../../__mocks__/handlers';
 
+const ENDPOINT_UUID = 'some-uuid';
+
 describe('compute.endpoints', () => {
   test('getAll', async () => {
     const {
@@ -58,6 +60,73 @@ describe('compute.endpoints', () => {
     const {
       req: { url, method, headers },
     } = await mirror(await endpoints.remove('some-uuid'));
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+});
+
+describe('next', () => {
+  test('getAll', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await endpoints.next.getAll());
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+
+  test('get', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await endpoints.next.get({ endpoint_uuid: ENDPOINT_UUID }));
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+
+  test('getStatus', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await endpoints.next.getStatus({ endpoint_uuid: ENDPOINT_UUID }));
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+
+  test('update', async () => {
+    const {
+      req: { url, method, headers, json },
+    } = await mirror(
+      await endpoints.next.update({
+        endpoint_uuid: ENDPOINT_UUID,
+        request: {
+          data: {
+            display_name: 'New Display Name',
+          },
+        },
+      }),
+    );
+    expect({
+      url,
+      method,
+      headers,
+      json,
+    }).toMatchSnapshot();
+  });
+
+  test('delete', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await endpoints.next.remove({ endpoint_uuid: ENDPOINT_UUID }));
     expect({
       url,
       method,
