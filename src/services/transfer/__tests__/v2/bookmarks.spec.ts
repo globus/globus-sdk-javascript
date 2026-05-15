36d33f3dc0d7173a403ae142adca25c7b8ccc443
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2026-05-15 08:20:09 -0500

    feat(Transfer): Adds `transfer.v2.bookmarks` integration (#835)
    - Updates the Transfer OpenAPI-generated types using `integration`
    (forward-looking).
    - Adds `transfer.v2.bookmarks.getList,get,update,remove`
    - Adds **Service Version Support** documentation
    
    <img width="1010" height="517" alt="Screenshot 2026-05-07 at 2 12 45 PM"
    src="https://github.com/user-attachments/assets/1ff4a456-3e3e-41b9-87f1-0dee9453ae02"
    />

---
+++ src/services/transfer/__tests__/v2/bookmarks.spec.ts
@@ -0,0 +1,100 @@
+import { v2 } from '../../index';
+import { mirror } from '../../../../__mocks__/handlers';
+
+describe('bookmarks', () => {
+  test('getAll', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await v2.bookmarks.getAll());
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+
+  test('create', async () => {
+    const {
+      req: { url, method, headers, json },
+    } = await mirror(
+      await v2.bookmarks.create({
+        request: {
+          data: {
+            data: {
+              type: 'Bookmark',
+              attributes: {
+                name: "35-KS&aIO(Fcdm%Y:2-c\\fp>(>[]i+R3*G'%imO1VoV;y-@QogK`.;xr:3<`B\\?kNH*$]_Lup",
+                path: 'XQE%qoM~mcP)TI f]RJ*#r-GDy~c\\N/',
+                pinned: false,
+              },
+              relationships: {
+                collection: {
+                  data: {
+                    type: 'Collection',
+                    id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
+                  },
+                },
+              },
+            },
+          },
+        },
+      }),
+    );
+    expect({
+      url,
+      method,
+      headers,
+      json,
+    }).toMatchSnapshot();
+  });
+
+  test('get', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await v2.bookmarks.get({ bookmark_id: 'my-bookmark-id' }));
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+
+  test('update', async () => {
+    const {
+      req: { url, method, headers, json },
+    } = await mirror(
+      await v2.bookmarks.update({
+        bookmark_id: 'my-bookmark-id',
+        request: {
+          data: {
+            data: {
+              type: 'Bookmark',
+              attributes: {
+                name: 'New Name',
+                path: 'new/path',
+                pinned: true,
+              },
+            },
+          },
+        },
+      }),
+    );
+    expect({
+      url,
+      method,
+      headers,
+      json,
+    }).toMatchSnapshot();
+  });
+
+  test('remove', async () => {
+    const {
+      req: { url, method, headers },
+    } = await mirror(await v2.bookmarks.remove({ bookmark_id: 'my-bookmark-id' }));
+    expect({
+      url,
+      method,
+      headers,
+    }).toMatchSnapshot();
+  });
+});
